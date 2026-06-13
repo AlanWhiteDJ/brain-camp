fb0e85474adeea2ca196e153cf83713729a28eca
AlanWhiteDJ <xclstar@163.com>
2026-06-13 10:00:40 +0800

    Remove service worker, force HTML renderer

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -36,8 +36,5 @@ if (!window._flutter) {
 _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19cab","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{"compileTarget":"dart2js","renderer":"html","mainJsPath":"main.dart.js"}]};
 
 _flutter.loader.load({
-  config: { renderer: "html" },
-  serviceWorkerSettings: {
-    serviceWorkerVersion: "1155655660" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
-  }
+  config: { renderer: "html" }
 });
